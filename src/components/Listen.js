25b617c37a7112e5aee361745d6f2b0c59058e3d
Jennifer Perez <jenniferperez@pursuit.org>
2020-05-29 17:22:36 -0400

    changed html for video card so i can later map

--- src/components/Listen.js
+++ src/components/Listen.js
@@ -7,22 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Listen=(props)=>{
 return(
   <>
-  {/* <div class="listen">
-  
-          
-  <div class="name">
-    Martinez Brothers
-  </div> */}
+
   <div class="container">
   <div class="row">
-  
+    <div class="col">
       Martinez Brothers
-   
     </div>
-    </div>
+   </div>
+   </div>
 
   <div class="container">
   <div class="row row-cols-4">
+
+    {/* mapme */}
     <div class="col">
     <div class="card" style={{width: "18rem"}}>
   <img src="..." class="card-img-top" alt="..."/>
@@ -31,27 +28,11 @@ return(
   </div>
   </div>
     </div>
-    <div class="col"><div class="card" style={{width: "18rem"}}>
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  </div>
-  </div>
-    <div class="col"><div class="card" style={{width: "18rem"}}>
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  </div></div>
-    <div class="col"><div class="card" style={{width: "18rem"}}>
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  </div></div>
+    {/* --- */}
+
   </div>
 </div>
+
 
   
 

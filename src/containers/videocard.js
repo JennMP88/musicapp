f0e4b17d1371b07df1402495da6b44526e2497f3
Jennifer Perez <jenniferperez@pursuit.org>
2020-05-29 19:42:51 -0400

    deleted unnecessary files and comments

--- src/containers/videocard.js
+++ src/containers/videocard.js
@@ -7,8 +7,6 @@ const VideoCard = ({ image, title, id }) => {
   return (
 
 
-
-    
     <div className='col-6'>
       <Link to={'/video/'+id}><img src={image} alt='' style={{width: '100%', height: 'auto'}} /></Link>
       <h5>{title}</h5>

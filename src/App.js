5b22a49a3a714b6d707b11565fdf6a9ae5429b23
Jennifer Perez <jenniferperez@pursuit.org>
2020-05-31 23:15:25 -0400

    fixed random artist showing diff videos issue

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import React, {Component} from 'react';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 //pages/components
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
-import Listen from "./components/Listen"
-import Genres from "./components/Genres"
-import Footer from "./components/Footer"
-import VideoPlayer from "./components/videoplayer"
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Listen from "./components/Listen";
+import Genres from "./components/Genres";
+import Footer from "./components/Footer";
+import VideoPlayer from "./components/videoplayer";
 
-
-
-
-class App extends Component{
- 
-render(){
-  return(
-   
-    <HashRouter>
-      <Route path='/' component={Navbar}/>
-      <Switch>
-      <Route exact path='/' component={Main}/>
-      <Route exact path='/listen' component={Listen}/>
-
-      <Route exact path='/listen/:id' component={VideoPlayer}/>
-
-      <Route exact path='/genres' component={Genres}/> 
-      </Switch>
-      <Route path='/' component={Footer}/>
-     </HashRouter>
-      
-    
- 
-  )
-}
-
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <Route path="/" component={Navbar} />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/listen" component={Listen} />
+          <Route exact path="/listen/:id" component={VideoPlayer} />
+          <Route exact path="/genres" component={Genres} />
+        </Switch>
+        <Route path="/" component={Footer} />
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/video.js
+++ src/components/video.js
@@ -6,6 +6,7 @@ const VideoDisplay = ({id}) => {
   const link = `https://www.youtube.com/embed/${id}?autoplay=1&fs=1&origin=http://localhost:3002`;
   console.log('myid',id)
   return (
+    
     <iframe title='yt-video' type="text/html" width="640" height="360"
      src={link} frameBorder="0"></iframe>
   );

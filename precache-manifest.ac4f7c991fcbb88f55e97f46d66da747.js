56cfb1ffdca92d731f82ca915ff6639bd5723832
Jennifer Perez <jenniferperez@pursuit.org>
2021-11-21 18:23:30 -0600

    Updates

---
+++ precache-manifest.ac4f7c991fcbb88f55e97f46d66da747.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4175903da64bd67647a46450be1972ea",
+    "url": "/JennMP88/musicapp/index.html"
+  },
+  {
+    "revision": "9c5691ca55a13a156ed4",
+    "url": "/JennMP88/musicapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "65698fdb5776cc044818",
+    "url": "/JennMP88/musicapp/static/css/main.6863c08c.chunk.css"
+  },
+  {
+    "revision": "9c5691ca55a13a156ed4",
+    "url": "/JennMP88/musicapp/static/js/2.1dbcc47c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JennMP88/musicapp/static/js/2.1dbcc47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65698fdb5776cc044818",
+    "url": "/JennMP88/musicapp/static/js/main.1fbd40db.chunk.js"
+  },
+  {
+    "revision": "0f4f559f0da284454376",
+    "url": "/JennMP88/musicapp/static/js/runtime-main.baa73219.js"
+  },
+  {
+    "revision": "5559c597a65e04aeba02a325e83306e4",
+    "url": "/JennMP88/musicapp/static/media/headphones.5559c597.jpg"
+  },
+  {
+    "revision": "91da7eb419f26fc7c33b58b14e4b4257",
+    "url": "/JennMP88/musicapp/static/media/record.91da7eb4.jpeg"
+  },
+  {
+    "revision": "828177fd815919724fb0a379acfb75a5",
+    "url": "/JennMP88/musicapp/static/media/twitter.828177fd.png"
+  }
+]);
